d584db1cdbb6ef9127a8c3a753109c99972ad7f3
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-09 17:39:08 -0600

    Toggle desktop menu done with js

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log("Hello World")
+const cartButton = document.querySelector(".navbar-shopping-cart")
+const desktopMenu = document.querySelector(".desktop-menu")
+
+cartButton.addEventListener("click", () => desktopMenu.classList.toggle("hidden"))

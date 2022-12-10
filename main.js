e48557773f0e2960088add283fa422962fe28ff3
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-09 18:30:32 -0600

    hamburguer button working with css and js

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-const cartButton = document.querySelector(".navbar-shopping-cart")
 const desktopMenu = document.querySelector(".desktop-menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
-cartButton.addEventListener("click", () => desktopMenu.classList.toggle("hidden"))
+const cartButton = document.querySelector(".navbar-shopping-cart")
+const hamburguerButton = document.querySelector(".hamburguer-btn")
+
+const menuTriggers = [
+    {
+        id: "desktop",
+        menu: desktopMenu,
+        button: cartButton,
+    },
+    {
+        id: "mobile",
+        menu: mobileMenu,
+        button: hamburguerButton,
+    }
+]
+
+menuTriggers.forEach(trigger => {
+    trigger.button.addEventListener("click", () => trigger.menu.classList.toggle("hidden"))
+})
